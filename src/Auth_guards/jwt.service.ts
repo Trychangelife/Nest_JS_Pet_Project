0d47aae276eed2c61c136698dfa53c44d8f32d23
Trychangelife <zkonstantinov@bk.ru>
2022-07-29 08:56:17 +0700

    Update jwt.service.ts

--- src/Auth_guards/jwt.service.ts
+++ src/Auth_guards/jwt.service.ts
@@ -1,8 +1,9 @@
-import { RefreshTokenStorageType, UsersType } from "../types/Types";
+
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Injectable } from "@nestjs/common";
 import { JwtService} from '@nestjs/jwt'
+import { RefreshTokenStorageType, UsersType } from "src/types/types";
 
 
 @Injectable()
