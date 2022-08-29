467410d39f9d0c47024e61f39ea76cec5b5a9ee5
Trychangelife <zkonstantinov@bk.ru>
2022-08-29 13:59:24 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
-  app.useGlobalPipes(new ValidationPipe({
-    exceptionFactory: (errors) => {
-      const errorsForResponse = []
-      errors.forEach((el) => {
-        const constraintsKeys = Object.keys(el.constraints)
-        constraintsKeys.forEach(ckey => {
-          errorsForResponse.push({message: el.constraints[ckey], field: el.property})
-        });
-      throw new BadRequestException(errorsForResponse)
-      })
-    }
-  }))
+  // app.useGlobalPipes(new ValidationPipe({
+  //   exceptionFactory: (errors) => {
+  //     const errorsForResponse = []
+  //     errors.forEach((el) => {
+  //       const constraintsKeys = Object.keys(el.constraints)
+  //       constraintsKeys.forEach(ckey => {
+  //         errorsForResponse.push({message: el.constraints[ckey], field: el.property})
+  //       });
+  //     throw new BadRequestException(errorsForResponse)
+  //     })
+  //   }
+  // }))
   app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(process.env.PORT);
   console.log(`Server listening on port: 5000`);
