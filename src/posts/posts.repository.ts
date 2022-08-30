234c6f51f4b7308e909ad90f9e0fc77cd20d7737
Trychangelife <zkonstantinov@bk.ru>
2022-08-30 15:42:57 +0700

    Update posts.repository.ts

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -65,7 +65,7 @@ async targetPosts(postId: string, userId?: string): Promise<object | undefined> 
         return undefined
     }
     else {
-        return {...targetPostWithAggregation[0], extendedLikesInfo: {...targetPostWithAggregation[0].extendedLikesInfo, newestLikes: targetPostWithAggregation[0].extendedLikesInfo.newestLikes.slice(0,3)
+        return {...targetPostWithAggregation[0], extendedLikesInfo: {...targetPostWithAggregation[0].extendedLikesInfo, newestLikes: targetPostWithAggregation[0].extendedLikesInfo.newestLikes.slice(0,3).reverse()
             //.sort((a,b) => a.addedAt.getTime() - b.addedAt.getTime())
         }}; 
         try {
