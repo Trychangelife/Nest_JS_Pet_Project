014aaa26d5602e02a26af2ddcce9a4f0100d6287
Trychangelife <zkonstantinov@bk.ru>
2022-08-31 15:02:54 +0700

    32/5

--- src/exception_filters/exception_filter.ts
+++ src/exception_filters/exception_filter.ts
@@ -1,37 +1,37 @@
-// import { ExceptionFilter, Catch, ArgumentsHost, HttpException, NotFoundException } from '@nestjs/common';
-// import { Request, Response } from 'express';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, NotFoundException } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 
-// @Catch(HttpException)
-// export class HttpExceptionFilter implements ExceptionFilter {
-//   catch(exception: HttpException, host: ArgumentsHost) {
-//     const ctx = host.switchToHttp();
-//     const response = ctx.getResponse<Response>();
-//     const request = ctx.getRequest<Request>();
-//     const status = exception.getStatus();
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
 
-//     if (status === 400) {
-//         const errorResponse = {
-//             errorsMessage: []
-//         }
-//         const responseBody: any = exception.getResponse()
-//         responseBody.message.forEach(element => 
-//             {errorResponse.errorsMessage.push(element)
+    if (status === 400) {
+        const errorResponse = {
+            errorsMessage: []
+        }
+        const responseBody: any = exception.getResponse()
+        responseBody.message.forEach(element => 
+            {errorResponse.errorsMessage.push(element)
             
-//         });
-//         response.status(status).json(errorResponse)
-//     }
-//     else if (status === 401) {
-//         response.status(status).json({message: "check input data or JWT Token"})
-//     }
-//     else {
-//         response
-//         .status(status)
-//         .json({
-//           statusCode: status,
-//           timestamp: new Date().toISOString(),
-//           path: request.url,
-//         });
-//     }
-//   }
-// }
+        });
+        response.status(status).json(errorResponse)
+    }
+    else if (status === 401) {
+        response.status(status).json({message: "check input data or JWT Token"})
+    }
+    else {
+        response
+        .status(status)
+        .json({
+          statusCode: status,
+          timestamp: new Date().toISOString(),
+          path: request.url,
+        });
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-
-
+import { HttpExceptionFilter } from './exception_filters/exception_filter';
+import { BadRequestException, HttpStatus, NotFoundException, ParseUUIDPipe, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors) => {
+      const errorsForResponse = []
+      errors.forEach((el) => {
+        const constraintsKeys = Object.keys(el.constraints)
+        constraintsKeys.forEach(ckey => {
+          errorsForResponse.push({message: el.constraints[ckey], field: el.property})
+        });
+      throw new BadRequestException(errorsForResponse)
+      })
+    }
+  }))
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(process.env.PORT);
   console.log(`Server listening on port: 5000`);
 }
 bootstrap();
 
-//import { BadRequestException, HttpStatus, NotFoundException, ParseUUIDPipe, ValidationPipe } from '@nestjs/common';
-//import { HttpExceptionFilter } from './exception_filters/exception_filter';
 
-  // app.useGlobalPipes(new ValidationPipe({
-  //   exceptionFactory: (errors) => {
-  //     const errorsForResponse = []
-  //     errors.forEach((el) => {
-  //       const constraintsKeys = Object.keys(el.constraints)
-  //       constraintsKeys.forEach(ckey => {
-  //         errorsForResponse.push({message: el.constraints[ckey], field: el.property})
-  //       });
-  //     throw new BadRequestException(errorsForResponse)
-  //     })
-  //   }
-  // }))
-  // app.useGlobalFilters(new HttpExceptionFilter())
+
+
+ 
