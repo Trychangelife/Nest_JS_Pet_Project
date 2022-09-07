6b8a7ea963f41b38f7a181ded66d2b9b3ef89369
Trychangelife <zkonstantinov@bk.ru>
2022-09-07 21:30:42 +0700

    Create posts.sql.repository.ts

---
+++ src/posts/posts.sql.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common"
+import { BloggersType } from "src/types/types"
+
+
+@Injectable()
+export class PostsRepositorySql {
+
+    constructor() {
+        
+    }
+
+}
