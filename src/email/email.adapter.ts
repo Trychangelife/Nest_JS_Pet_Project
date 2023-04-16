ad970b5399d87dafceb38e4ccf3563ad57dc528c
Evgeniy <zkonstantinov@bk.ru>
2023-04-16 14:57:58 +0300

    Update email.adapter.ts

--- src/email/email.adapter.ts
+++ src/email/email.adapter.ts
@@ -7,47 +7,16 @@ export class EmailAdapter  {
 
     constructor (private mailerService: MailerService) {}
     async sendEmailConfirmation (email: string, message: string, subject: string): Promise<object> {
-
-
-
-      try {
         return await this.mailerService.sendMail({
-          from: 'Evgeniy <jenbka999@gmail.com>',
-          to: email,
-          subject: subject,
-          html: message
-      })
-      } catch (error) {
-        throw new HttpException(
-          `Ошибка работы почты: ${JSON.stringify(error)}`,
-          HttpStatus.UNPROCESSABLE_ENTITY,
-        );
-      }
-    //     return this.mailerService.sendMail({
-    //     from: 'Evgeniy <jenbka999@gmail.com>',
-    //     to: email,
-    //     subject: subject,
-    //     html: message
-    // }).catch((e) => {
-    //   throw new HttpException(
-    //     `Ошибка работы почты: ${JSON.stringify(e)}`,
-    //     HttpStatus.UNPROCESSABLE_ENTITY,
-    //   );
-    // });
+        from: 'Evgeniy <jenbka999@gmail.com>',
+        to: email,
+        subject: subject,
+        html: message
+    }).catch((e) => {
+      throw new HttpException(
+        `Ошибка работы почты: ${JSON.stringify(e)}`,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    });
     }
 } 
-
-
-// try {
-//   await this.mailerService.sendMail({
-//     from: 'Evgeniy <jenbka999@gmail.com>',
-//     to: email,
-//     subject: subject,
-//     html: message
-// })
-// } catch (error) {
-//   throw new HttpException(
-//     `Ошибка работы почты: ${JSON.stringify(e)}`,
-//     HttpStatus.UNPROCESSABLE_ENTITY,
-//   );
-// }
