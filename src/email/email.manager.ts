3b6a61e6ab3010368de9d15a9a087dcecfeb7d8c
Evgeniy <zkonstantinov@bk.ru>
2023-04-16 15:28:55 +0300

    update templates

--- src/email/email.manager.ts
+++ src/email/email.manager.ts
@@ -9,18 +9,11 @@ export class EmailManager  {
 
     async sendEmailConfirmation (user: UsersType): Promise<object> {
         const url = "https://nest-js-pet-project-fawn.vercel.app/auth/registration-confirmation"
-        const message = `Please click this link to confirm you email: <a href="${url}?code=${user.emailConfirmation.codeForActivated}">${url}?code=${user.emailConfirmation.codeForActivated}</a>`
+        const message1 = `Please click this link to confirm you email: <a href="${url}?code=${user.emailConfirmation.codeForActivated}">${url}?code=${user.emailConfirmation.codeForActivated}</a>`
+        const message = `<h1>Thank for your registration</h1>
+        <p>To finish registration please follow the link below:
+            <a href='${url}?code=${user.emailConfirmation.codeForActivated}'>complete registration</a>
+        </p>`
         return this.emailAdapter.sendEmailConfirmation(user.email, message, 'Email Confirmation after registration')
     }
-    // async sendEmailConfirmation(user: UsersType) {
-    //     const url = "https://nest-js-pet-project-fawn.vercel.app/auth/registration-confirmation"
-    //     const mail = {
-    //         to: user.email,
-    //         subject: 'Evgeniy <jenbka999@gmail.com>',
-    //         from: '<send_grid_email_address>',
-    //         text: `Please click this link to confirm you email: <a href="${url}?code=${user.emailConfirmation.codeForActivated}">${url}?code=${user.emailConfirmation.codeForActivated}</a>`
-    //     };
-
-    //     return await this.sendgridService.send(mail);
-    // }
 }
